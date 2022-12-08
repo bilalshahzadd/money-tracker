196c0ad37c8ff1ce0a4b48b6f6cdd13fa3ea6341
Bilal Shahzad <89092210+bilalshahzadd@users.noreply.github.com>
2022-12-08 06:42:58 +0500

    local storage

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -26,11 +26,31 @@ export const Main = () => {
         setExtras(event.target.valueAsNumber);
     }
 
+    const saveInformation = () => {
+        const data = {
+            income: income,
+            houseExpense: houseExpense,
+            travelExpense: travelExpense,
+            extras: extras,
+            totalExpense: totalExpense,
+            totalSavings: totalSavings
+        }
+
+        localStorage.setItem('items', JSON.stringify(data));
+    }
+
+    const fetchInformation = () => {
+        const data = localStorage.getItem('items');
+        const convertedString = JSON.stringify(data);
+    }
+
+    fetchInformation();
+
     return (
         <>
 
-            <div className="w-full border-[#121212] bg-[#282c34] h-[520px] flex justify-evenly">
-                <div className="w-5/12 border-2 border-[#f2f2f2] rounded-[15px] h-[27rem] mt-10 p-5">
+            <div className="w-full border-[#121212] bg-[#282c34] h-[520px] flex justify-evenly max-[600px]:pt-3 max-[600px]:flex-col max-[600px]:h-auto">
+                <div className="w-5/12 border-2 border-[#f2f2f2] rounded-[8px] h-[27rem] mt-10 p-5 max-[600px]:w-11/12 max-[600px]:m-auto">
                     <div className="flex flex-col gap-2 text-[#f2f2f2]">
                         <h2 className="text-3xl font-bold">Make Your Budget</h2>
                         <div className="flex flex-col gap-2">
@@ -57,32 +77,35 @@ export const Main = () => {
                         </div>
                     </div>
                 </div>
-                <div className={`w-5/12 border-2 border-[#f2f2f2] rounded-[15px] h-[27rem] mt-10 p-5`}>
+                <div className="w-5/12 border-2 border-[#f2f2f2] rounded-[8px] h-[27rem] mt-10 p-5 max-[600px]:w-11/12 max-[600px]:mt-3 max-[600px]:m-auto max-[600px]:mb-10">
                     <div className="flex flex-col gap-5 text-[#f2f2f2]">
                         <h2 className="text-3xl font-bold">Your Expenses</h2>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Your Income:</p>
-                            <p className="text-l font-semibold">${!income ? " Income" : income}</p>
+                            <p className="text-l font-semibold">${!income ? " " : income}</p>
                         </div>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Your Household Expenses:</p>
-                            <p className="text-l font-semibold">${!houseExpense ? " House Expense" : houseExpense}</p>
+                            <p className="text-l font-semibold">${!houseExpense ? " " : houseExpense}</p>
                         </div>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Your Travel Expenses:</p>
-                            <p className="text-l font-semibold">${!travelExpense ? " Travel Expense" : travelExpense}</p>
+                            <p className="text-l font-semibold">${!travelExpense ? " " : travelExpense}</p>
                         </div>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Extras:</p>
-                            <p className="text-l font-semibold">${!extras ? " Extras" : extras}</p>
+                            <p className="text-l font-semibold">${!extras ? " " : extras}</p>
                         </div>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Total Expense:</p>
-                            <p className="text-l font-semibold">${!totalExpense ? " Total Expenses" : totalExpense}</p>
+                            <p className="text-l font-semibold">${!totalExpense ? " " : totalExpense}</p>
                         </div>
                         <div className="flex gap-2 items-center">
                             <p className="text-xl font-bold">Total Saving:</p>
-                            <p className="text-l font-semibold">${!totalSavings ? " Total Savings" : totalSavings}<span className="text-[#FF0000]">{totalExpense > income ? " You are in debt" : " "}</span></p>
+                            <p className="text-l font-semibold">${!totalSavings ? " " : totalSavings}<span className="text-[#FF0000]">{totalExpense > income ? " You are in debt" : " "}</span></p>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <button className="border-2 border-[#f2f2f2] rounded-[5px] bg-[#f2f2f2] text-[#282c34] py-[5px] px-[40px]" onClick={saveInformation}>Save Information</button>
                         </div>
                     </div>
                 </div>
